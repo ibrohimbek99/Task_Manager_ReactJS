06e2f8d6030573904705d4ad79a2e4a85591621c
Ibrahim Pulatov <ibrohimbek1999@gmail.com>
2022-10-02 22:02:22 -0400

    Final Commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <div>
+      <nav className="nav">
+        <div className="nav-left">
+          <a className="brand" href="/">
+            Task Manager
+          </a>
+        </div>
+        <div className="nav-right">
+          <div className="tabs">
+            <a href="instagram.com">IP & JA</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
